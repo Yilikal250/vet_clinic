7ac48a5e54fb88103b31c6602d2246432b34b502
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 22:40:49 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
  CREATE TABLE animals (
-vet_clinic(# id int,
-vet_clinic(# name VARCHAR(100),
-vet_clinic(# date_of_birth date,
-vet_clinic(# escape_attempts int,
-vet_clinic(# neutered BOOL,
-vet_clinic(# weight_kg DECIMAL);
-
+    id integer,
+    name text,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg numeric
+);
