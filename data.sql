76a7029d1add03958efb1e9666ab3981cb4d68c5
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 22:57:33 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,6 @@
-
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES
+('Agumon', DATE '2020-02-03', 0, 'TRUE', 10.23),
+('Gabumon', DATE '2018-11-15', 2, 'TRUE', 8),
+('Pikachu', DATE '2021-01-07', 1, 'FALSE', 15.04),
+('Devimon', DATE '2017-05-12', 5, 'TRUE', 11);
