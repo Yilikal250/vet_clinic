7a93beeb67d5ec05f499230579733ab4c39ba43b
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 22:55:28 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,1 @@
-SELECT name FROM animals WHERE name LIKE '%mon';
-SELECT name, date_of_birth FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-SELECT name, neutered, escape_attempts FROM animals WHERE neutered = 'TRUE' AND escape_attempts < 3;
-SELECT name, date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attempts, weight_kg FROM animals WHERE weight_kg > 10.5;
-SELECT name, neutered FROM animals WHERE neutered = 'TRUE';
-SELECT name FROM animals WHERE NOT name = 'Gabumon';
-SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
