383404fc3218320445e6083a9f3bce172139bc47
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 20:53:23 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -2,3 +2,4 @@ select * from animals where name LIKE  '%mon';
 SELECT * FROM animals WHERE date_of_birth BETWEEN '#01-01-2016#' AND '#01-01-2019#';
 SELECT name FROM animals WHERE neutered = 't' AND escape_attempts < '3';
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg < '10.5';
