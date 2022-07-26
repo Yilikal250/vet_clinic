1b5fa272815188b3878da2b140352f0055de86d2
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 13:29:08 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Agumon','03 03 2023','true','0',10.23kg');
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Gabumon','11 15 2018','true','2','8kg');
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Pikachu','1 1 2021','false','1','15.04kg');
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES ('Devimon','5 12 2017','true','5','11kg');
