8f402a08b9296af1df7c7fa756756d8005e0478d
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 13:03:46 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,8 @@
+ CREATE TABLE animals (
+vet_clinic(# id integer,
+vet_clinic(# name VARCHAR,
+vet_clinic(# date_of_birth date,
+vet_clinic(# escape_attempts integer,
+vet_clinic(# neutered boolean,
+vet_clinic(# weight_kg decimal );
 
