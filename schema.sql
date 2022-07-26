dfcfcb2f8f6001db0d0d656a226d613d0ec0ca60
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-26 21:01:15 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
  CREATE TABLE animals (
-vet_clinic(# id integer,
-vet_clinic(# name VARCHAR,
+vet_clinic(# id int,
+vet_clinic(# name VARCHAR(100),
 vet_clinic(# date_of_birth date,
-vet_clinic(# escape_attempts integer,
-vet_clinic(# neutered boolean,
-vet_clinic(# weight_kg decimal );
+vet_clinic(# escape_attempts int,
+vet_clinic(# neutered BOOL,
+vet_clinic(# weight_kg DECIMAL);
 
