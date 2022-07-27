a893c173a81edb2d6af072fbd5d3518ecf0d65a5
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-27 22:46:54 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -34,3 +34,27 @@ ADD COLUMN owner_id INT;
 
 ALTER TABLE animals
 ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INT,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    vet_id INT REFERENCES vets (id),
+    species_id INT REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals (id),
+    vet_id INT REFERENCES vets (id),
+    date_of_visit DATE
+);
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX visits_animal_id_idx ON visits(animal_id);
+CREATE INDEX visits_vet_id_idx ON visits(vet_id);
+CREATE INDEX owners_email_idx ON owners(email);
