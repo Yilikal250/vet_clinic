76673d38778a397a2699365a96fd20b3da747844
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-27 20:15:07 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -14,3 +14,12 @@ VALUES
 ('Boarmon', DATE '2005-06-07', 7, 'TRUE', 20.4),
 ('Blossom', DATE '1998-10-13', 3, 'TRUE', 17),
 ('Ditto', DATE '2022-05-14', 4, 'TRUE', 22);
+
+INSERT INTO owners (full_name, age)
+VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
