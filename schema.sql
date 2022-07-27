8862040f20dadc1778d002e05896b71ca2a20bb3
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-27 20:00:41 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -8,3 +8,29 @@
 );
 ALTER TABLE animals
 ADD COLUMN species TEXT;
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT,
+    age INT
+);
+
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+ALTER TABLE animals 
+DROP COLUMN id, DROP COLUMN species;
+
+ALTER TABLE animals 
+ADD COLUMN id SERIAL PRIMARY KEY, ADD COLUMN species_id INT;
+
+ALTER TABLE animals 
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
