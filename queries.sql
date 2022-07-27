119a7d5a3da63cffa9c7b996f7a25a565bddc5a6
Yilikal Teshome <97841835+Yilikal250@users.noreply.github.com>
2022-07-27 12:31:05 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,29 @@ SELECT name, escape_attempts, weight_kg FROM animals WHERE weight_kg > 10.5;
 SELECT name, neutered FROM animals WHERE neutered = 'TRUE';
 SELECT name FROM animals WHERE NOT name = 'Gabumon';
 SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > DATE '2022-01-01';
+SAVEPOINT SP01;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP01;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT * FROM animals;
